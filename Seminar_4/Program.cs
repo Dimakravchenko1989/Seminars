e40a7cea2fd2daf6a95e42fbc6ee79f7a267dd7b
Dmitriy <dimakravchenko1989@yandex.ru>
2022-11-10 02:12:07 +0300

    Добавил решение 4 задач

--- Seminar_4/Program.cs
+++ Seminar_4/Program.cs
@@ -56,12 +56,34 @@ Console.Write($"Factorial = {Factorial(a)}");
 
 
 
-// Задача 4. 
+// Задача 4. Напишите программу, которая выводит массив из 8 элементов, заполненый 0 и 1 в случайном порядке.
+/*
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
 
+    for(int i = 0; i < size; i++)
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    
+    return array;
+}
 
+void ShowArray(int[] array)
+{
+    Console.Write("Array is: ");
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
 
+    Console.WriteLine(" \n");
+}
 
+Console.Write("Input a number of elements: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a min possible value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a max possible: ");
+int max = Convert.ToInt32(Console.ReadLine());
 
-
-
-
+int[]myArray = CreateRandomArray(n,min,max);
+ShowArray(myArray);
+*/
