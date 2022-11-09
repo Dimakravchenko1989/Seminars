bb4e0473399193b62a92c14b1095190daf65acc5
Dmitriy <dimakravchenko1989@yandex.ru>
2022-11-10 01:11:51 +0300

    Добавил решение 3 задач

---
+++ Seminar_4/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задача 1. Напишите программу, которая принимает на вход число N и выдает сумму чисел от 1 до числа N.
+/*
+int FindSum(int number)
+{
+    int sum = 0;
+
+    for(int current = 1;current <= number; current++)
+        sum +=current;
+
+
+    return sum;
+}
+
+Console.Write("Input a number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Sum of numbers from 1 to {a} is {FindSum(a)}"); 
+*/
+
+
+
+// Задача 2. Напишите программу, которая принимает на вход число и выдает количество цифр в числе.
+/*
+int NumberOfDigit(int number)
+{
+    int count = 0;
+    while(number != 0)
+    {
+        number /= 10;
+        count++;
+    }
+    return count;
+}
+Console.WriteLine("Input number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Number of digit {NumberOfDigit(a)}");
+*/
+
+
+
+// Задача 3. Напишите программу, которая принимает на вход число N и выдает произведение чисел от 1 до N.
+/*
+long Factorial (int n)
+{
+    long sum = 1;
+    for(int current = 1; current <= n; current++)
+    {
+        sum *= current;
+    }
+    return sum;
+}
+Console.Write("Input number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Factorial = {Factorial(a)}");
+*/
+
+
+
+// Задача 4. 
+
+
+
+
+
+
+
+
